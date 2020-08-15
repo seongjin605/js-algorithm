cce5d424c9d1dfb78ec225f524c565f7140674cb
preciouStar <seongjin605@users.noreply.github.com>
2020-08-15 12:12:41 +0900

    test

---
+++ programmers/SkillTree.js
@@ -0,0 +1,27 @@
+function solution(skill, skill_trees) {
+  var answer = 0;
+
+  var count = 0;
+
+  var test = new Array(skill_trees.length);
+  for (var i = 0; i < skill_trees.length; i++) {
+    var skills = skill_trees[i].split(''); // 스킬트레 순서 반환
+    var inputSkillSet = skill.split(''); // 스킬셋 순서 반환
+
+    var result = [];
+    while (inputSkillSet.length > 0) {
+      var first = inputSkillSet.shift();
+      if (skills.includes(first)) {
+        result.push(skills.shift()); // 같으면 들어감
+      } else {
+        skills.shift();
+      }
+    }
+    console.log(skills);
+  }
+  // console.log(skillSet);
+
+  return answer;
+}
+
+console.log(solution('CBD', ['BACDE', 'CBADF', 'AECB', 'BDA']));
