37602a8d6c4e3c2a8a98452222c732ede06a36d4
preciouStar <seongjin605@users.noreply.github.com>
2020-08-16 10:53:14 +0900

    test

--- programmers/SkillTree.js
+++ programmers/SkillTree.js
@@ -1,15 +1,12 @@
 function solution(skill, skill_trees) {
   var answer = 0;
 
-  var count = 0;
-
-  var test = new Array(skill_trees.length);
   for (var i = 0; i < skill_trees.length; i++) {
     var skills = skill_trees[i].split(''); // 스킬트레 순서 반환
     var inputSkillSet = skill.split(''); // 스킬셋 순서 반환
 
     var result = [];
-    while (inputSkillSet.length > 0) {
+    while (skills.length > 0) {
       var first = inputSkillSet.shift();
       if (skills.includes(first)) {
         result.push(skills.shift()); // 같으면 들어감
@@ -18,9 +15,16 @@ function solution(skill, skill_trees) {
       }
     }
     // result 결과를 통해 같은지 카운트
-    console.log(skills);
+    var skillResult = skill_trees[i].split(''); // 스킬트레 순서 반환
+    for (var i = 0; i < result.length; i++) {
+      if (
+        skill_trees[i].length > 0 &&
+        skillResult[i] == result[i] &&
+        skillResult[i + 1] == result[i + 1]
+      )
+        answer++;
+    }
   }
-  // console.log(skillSet);
 
   return answer;
 }
