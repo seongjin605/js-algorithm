f434953d60c4451a3fb4a5a339e352a00f26e5ff
preciouStar <seongjin605@users.noreply.github.com>
2020-08-18 23:59:20 +0900

    TC 100% https://www.hackerrank.com/challenges/mini-max-sum/submissions/code/175010147

--- hackerrank/MiniMaxSum.js
+++ hackerrank/MiniMaxSum.js
@@ -3,30 +3,40 @@ function miniMaxSum(arr) {
   if (arr.length === 0 || arr.length > Math.pow(10, 9))
     throw new Error("Not Invalid Argument");
 
+  arr.sort();
+
   let max = 0;
   let min = 0;
-  arr.sort();
+
   for (let i = 0; i < arr.length; i++) {
     max = arr[0];
     min = arr[0];
-    if (arr[i] >= max) {
+    if (arr[i] > max) {
       max = arr[i];
-    } else {
+    } else if (arr[i] < min) {
       min = arr[i];
     }
   }
   const minResult = arr
-    .filter((x) => x !== max)
-    .map((x) => x)
-    .reduce((accumulator, currentValue) => accumulator + currentValue);
+    .filter((x) => x !== max || max === min)
+    .reduce((accumulator, currentValue, currentIndex) => {
+      if (max === min && currentIndex === 1) {
+        return accumulator + currentValue - min;
+      }
+      return accumulator + currentValue;
+    });
 
   const maxResult = arr
-    .filter((x) => x !== min)
-    .map((x) => x)
-    .reduce((accumulator, currentValue) => accumulator + currentValue);
+    .filter((x) => x !== min || max === min)
+    .reduce((accumulator, currentValue, currentIndex) => {
+      if (max === min && currentIndex === 1) {
+        return accumulator + currentValue - min;
+      }
+      return accumulator + currentValue;
+    });
 
   console.log(minResult, maxResult);
 }
 
-const arr = [7, 69, 2, 221, 8974];
+const arr = [1, 2, 3, 4, 5];
 miniMaxSum(arr);
