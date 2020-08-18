dddea396a935135cffdeb4b67f6366719818679e
preciouStar <seongjin605@users.noreply.github.com>
2020-08-19 00:16:22 +0900

    TC 100% https://www.hackerrank.com/challenges/birthday-cake-candles/problem\?h_r\=next-challenge\&h_v\=zen

--- hackerrank/BirthdayCakeCandles.js
+++ hackerrank/BirthdayCakeCandles.js
@@ -1,7 +1,12 @@
 // Complete the birthdayCakeCandles function below.
 function birthdayCakeCandles(arr) {
+  if (arr.length === 0 || arr.length > Math.pow(10, 7))
+    throw new Error("Not Invalid Argument");
   let max = 0;
   let count = 0;
+  arr.sort(function (a, b) {
+    return b - a;
+  });
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] >= max) {
       max = arr[i];
@@ -11,4 +16,4 @@ function birthdayCakeCandles(arr) {
   return count;
 }
 
-console.log(birthdayCakeCandles([3, 2, 1, 3]));
+console.log(birthdayCakeCandles([18, 90, 90, 13, 90, 75, 90, 8, 90, 43]));
