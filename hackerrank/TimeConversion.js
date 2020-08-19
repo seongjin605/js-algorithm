819d9747f219e5e204998f01a01df4c9e26b2787
preciouStar <seongjin605@users.noreply.github.com>
2020-08-20 06:29:01 +0900

    Time conversion TC 100% https://www.hackerrank.com/challenges/time-conversion/submissions/code/175254134

--- hackerrank/TimeConversion.js
+++ hackerrank/TimeConversion.js
@@ -3,13 +3,15 @@ function timeConversion(s) {
   const result = splitedData.map((x, i, arr) => {
     const parsed = parseInt(x);
     if (i === 0) {
-      return s.toUpperCase().includes("PM") ? parsed + 12 : parsed;
+      const hour = parsed % 12;
+      return s.toUpperCase().includes("PM") || hour > 10
+        ? hour + 12
+        : "0" + hour;
     } else if (x.includes("PM") || x.includes("AM")) {
-      return parsed;
+      return parsed > 10 ? parsed : "0" + parsed.toString();
     }
-    return parsed > 10 ? parsed : "0" + parsed;
+    return parsed > 10 ? parsed : "0" + parsed.toString();
   });
-
   return `${result[0]}:${result[1]}:${result[2]}`;
 }
 
@@ -17,3 +19,4 @@ console.log(timeConversion("07:05:45PM"));
 console.log(timeConversion("06:55:55AM"));
 console.log(timeConversion("10:55:55AM"));
 console.log(timeConversion("10:55:55PM"));
+console.log(timeConversion("24:00:00AM"));
