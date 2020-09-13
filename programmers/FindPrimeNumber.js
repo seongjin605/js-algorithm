bab33002ead0f9821323e77305bdfa03112df9fc
preciouStar <seongjin605@users.noreply.github.com>
2020-09-14 00:39:51 +0900

    에라토스테네스 체

--- programmers/FindPrimeNumber.js
+++ programmers/FindPrimeNumber.js
@@ -1,37 +1,62 @@
+// function isPrime(n) {
+//   if (n < 2) {
+//     return false;
+//   }
+//   var sqrt = Math.sqrt(n);
+//   console.log('sqrt:', sqrt);
+//   for (var i = 2; i <= sqrt; i++) {
+//     console.log(`${n} % ${i} === ${n % i}`);
+//     if (n % i === 0) return false;
+//   }
+//   return true;
+// }
+
 // function solution(n) {
 //   var result = [];
-//   for (var i = 2; i < n; i++) {
-//     console.log(`${n} % ${i} === ${n % i}`);
-//     // 10 % 2 === 0
-//     // 10 % 3 === 1
-//     // 10 % 4 === 2
-//     if (n % i === 0) {
-//       continue;
+//   for (var i = 0; i < n; i++) {
+//     if (isPrime(i)) {
+//       result.push(i);
 //     }
-//     result.push(i);
 //   }
 //   console.log(result);
 // }
 
-function isPrime(n) {
-  if (n < 2) {
-    return false;
-  }
-  var sqrt = Math.sqrt(n);
-  for (var i = 2; i <= sqrt; i++) {
-    if (n % i === 0) return false;
-  }
-  return true;
-}
+// console.log(solution(10));
+
+// 2
 
 function solution(n) {
-  var result = [];
-  for (var i = 0; i < n; i++) {
-    if (isPrime(i)) {
-      result.push(i);
-    }
+  var flags = [false, false];
+
+  var until = n - flags.length;
+  for (var i = 0; i < until; i++) {
+    flags.push(true);
   }
-  console.log(result);
+
+  var count = 0;
+
+  var prime = 2;
+
+  while (prime <= Math.sqrt(n)) {
+    crossOff(flags, prime);
+
+    prime = getNextPrime(flags, prime);
+  }
+  console.log(flags);
 }
 
-console.log(solution(10));
+function crossOff(flags, prime) {
+  for (var i = prime * prime; i < flags.length; i += prime) {
+    flags[i] = false;
+  }
+}
+
+function getNextPrime(flags, prime) {
+  var next = prime + 1;
+  while (next < flags.length && !flags[next]) {
+    next++;
+  }
+  return next;
+}
+
+solution(10);
