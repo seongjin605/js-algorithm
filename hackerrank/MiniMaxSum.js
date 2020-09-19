0fbd78f422e6e45f648589017647536ef83744db
preciouStar <seongjin605@users.noreply.github.com>
2020-09-20 01:41:18 +0900

    update

--- hackerrank/MiniMaxSum.js
+++ hackerrank/MiniMaxSum.js
@@ -1,42 +1,69 @@
-// Complete the miniMaxSum function below.
+// function miniMaxSum(arr) {
+//   if (arr.length === 0 || arr.length > Math.pow(10, 9)) throw new Error('Not Invalid Argument');
+
+//   arr.sort();
+
+//   let max = 0;
+//   let min = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     max = arr[0];
+//     min = arr[0];
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     } else if (arr[i] < min) {
+//       min = arr[i];
+//     }
+//   }
+//   console.log('min:', min);
+//   console.log('max:', max);
+
+//   const minResult = arr
+//     .filter(x => x !== max || max === min)
+//     .reduce((accumulator, currentValue, currentIndex) => {
+//       if (max === min && currentIndex === 1) {
+//         return accumulator + currentValue - min;
+//       }
+//       return accumulator + currentValue;
+//     });
+
+//   const maxResult = arr
+//     .filter(x => x !== min || max === min)
+//     .reduce((accumulator, currentValue, currentIndex) => {
+//       if (max === min && currentIndex === 1) {
+//         return accumulator + currentValue - min;
+//       }
+//       return accumulator + currentValue;
+//     });
+
+//   console.log(minResult, maxResult);
+// }
+
 function miniMaxSum(arr) {
-  if (arr.length === 0 || arr.length > Math.pow(10, 9))
-    throw new Error("Not Invalid Argument");
+  if (arr.length === 0 || arr.length > Math.pow(10, 9)) throw new Error('Not Invalid Argument');
 
-  arr.sort();
+  const max = Math.max.apply(0, arr);
+  const min = Math.min.apply(0, arr);
 
-  let max = 0;
-  let min = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    max = arr[0];
-    min = arr[0];
-    if (arr[i] > max) {
-      max = arr[i];
-    } else if (arr[i] < min) {
-      min = arr[i];
-    }
+  if (min === max) {
+    console.log(min * (arr.length - 1), max * (arr.length - 1));
+    return;
   }
-  const minResult = arr
-    .filter((x) => x !== max || max === min)
-    .reduce((accumulator, currentValue, currentIndex) => {
-      if (max === min && currentIndex === 1) {
-        return accumulator + currentValue - min;
-      }
-      return accumulator + currentValue;
-    });
 
-  const maxResult = arr
-    .filter((x) => x !== min || max === min)
-    .reduce((accumulator, currentValue, currentIndex) => {
-      if (max === min && currentIndex === 1) {
-        return accumulator + currentValue - min;
-      }
-      return accumulator + currentValue;
-    });
+  const accumulatedMax = arr
+    .filter(val => val !== max)
+    .reduce((previousVal, currentVal) => {
+      return previousVal + currentVal;
+    }, 0);
 
-  console.log(minResult, maxResult);
+  const accumulatedMin = arr
+    .filter(val => val !== min)
+    .reduce((previousVal, currentVal) => {
+      return previousVal + currentVal;
+    }, 0);
+
+  console.log(accumulatedMax, accumulatedMin);
 }
 
-const arr = [1, 2, 3, 4, 5];
-miniMaxSum(arr);
+miniMaxSum([1, 2, 3, 4, 5]);
+miniMaxSum([5, 5, 5, 5, 5]);
