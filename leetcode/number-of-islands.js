d6b564c677e89da30082e62fceda10e392effa1a
preciouStar <seongjin605@users.noreply.github.com>
2020-10-15 01:06:46 +0900

    update

--- leetcode/number-of-islands.js
+++ leetcode/number-of-islands.js
@@ -1,40 +1,34 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
 var numIslands = function (grid) {
+  if (!grid || grid.length === 0 || grid[0].length === 0) return 0;
+
   let numIslands = 0;
 
-  if (!grid || grid.length === 0 || grid[0].length === 0) {
-    return numIslands;
-  }
-
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[0].length; j++) {
-      console.log('grid:', grid[i][j]);
-      if (grid[i][j] !== '1') {
-        continue;
+  for (let i = 0; i < grid.lengh; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === '1') {
+        numIslands += dfs(grid, i, j);
       }
-      numIslands++;
-      dfs(grid, i, j);
     }
   }
+
   return numIslands;
 };
 
 function dfs(grid, i, j) {
-  grid[i][j] = 'x';
+  if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0') {
+    return;
+  }
 
-  // right
-  if (j + 1 < grid[0].length < 0 && grid[i][j + 1] === '1') {
-    dfs(grid, i, j + 1);
-  }
-  // left
-  if (j - 1 >= 0 && grid[i][j - 1] === '1') {
-    dfs(grid, i, j - 1);
-  }
-  // up
-  if (i - 1 >= 0 && grid[i - 1][j] === '1') {
-    dfs(grid, i - 1, j);
-  }
-  // down
-  if (i + 1 < grid.length < 0 && grid[i + 1][j] === '1') {
-    dfs(grid, i + 1, j);
-  }
+  grid[i][j] = '0';
+
+  dfs(grid, i, j + 1); // right
+  dfs(grid, i, j - 1); // left
+  dfs(grid, i + 1, j); // up
+  dfs(grid, i - 1, j); // down
+
+  return 1;
 }
