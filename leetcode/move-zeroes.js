4d6d15067da95837fe597c4f5ca7fa271696fe93
preciouStar <seongjin605@users.noreply.github.com>
2020-10-16 21:21:04 +0900

    test

---
+++ leetcode/move-zeroes.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  const zeros = [];
+  while (nums.includes(0)) {
+    const indexOfZero = nums.findIndex(val => val === 0); // 0의 인덱스를 반환
+    if (indexOfZero !== -1) {
+      zeros.push(...nums.slice(indexOfZero, 1));
+    }
+  }
+  nums.push(...zeros);
+  return nums;
+};
