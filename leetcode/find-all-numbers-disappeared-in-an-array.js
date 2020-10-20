9856163e027025de41587f5fb5d6d40d0334b3d7
preciouStar <seongjin605@users.noreply.github.com>
2020-10-20 22:17:58 +0900

    update

--- leetcode/find-all-numbers-disappeared-in-an-array.js
+++ leetcode/find-all-numbers-disappeared-in-an-array.js
@@ -6,7 +6,6 @@ var findDisappearedNumbers = function (nums) {
   if (!nums || nums.length === 0) return [];
 
   let min = Math.min.apply(Math, nums); // 배열의 최솟값
-  // console.log('min:', min);
 
   const result = [];
 
@@ -14,11 +13,7 @@ var findDisappearedNumbers = function (nums) {
 
   nums = new Set(nums);
 
-  console.log('nums:', nums);
-
   nums.forEach((val, index) => {
-    console.log('val:', val);
-    console.log('min:', min);
     if (min !== val) {
       result.push(min);
     }
