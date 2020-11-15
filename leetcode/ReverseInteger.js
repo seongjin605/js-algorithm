e1c10ce36e8365622f07fa5601d999b4bda2c11c
preciouStar <seongjin605@users.noreply.github.com>
2020-11-15 23:33:34 +0900

    Imporve solution

--- leetcode/ReverseInteger.js
+++ leetcode/ReverseInteger.js
@@ -2,23 +2,8 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-  console.log('check:', x > Math.pow(2, 31) - 1);
-  if (!Number.isInteger(x) || x < -Math.pow(2, 31) || x > Math.pow(2, 31) - 1)
-    return;
-
-  const splited = ('' + x).toString().split('');
-
-  if (x > 0) {
-    return splited.reverse().join('');
-  }
-  const minus = splited.shift();
-  return minus + splited.reverse().join('');
+var reverse = function (x) {
+  const absReversed = Math.abs(x).toString().split('').reverse().join('');
+  if (absReversed > 2 ** 31) return 0;
+  return absReversed * Math.sign(x);
 };
-
-// console.log(reverse(1234));
-// console.log(reverse(-1234));
-console.log(reverse(1534236469));
-
-// 2147483648
-// 1534236469
