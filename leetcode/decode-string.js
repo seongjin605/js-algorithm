e409a46203f0646bf262efd258f62199490aac57
preciouStar <seongjin605@users.noreply.github.com>
2020-11-19 23:37:22 +0900

    updating solution

--- leetcode/decode-string.js
+++ leetcode/decode-string.js
@@ -4,6 +4,19 @@
  */
 var decodeString = function (s) {
   const splitedArr = s.split('');
-  console.log('splitedArr:', splitedArr);
+
   const result = [];
+
+  while (splitedArr.length > 0) {
+    const node = splitedArr.shift();
+
+    const isNanNumber = Number.isNaN(Number(node));
+
+    // 숫자타입이면
+    if (!isNanNumber) {
+      splitedArr.shift(); // 왼쪽 제거
+      splitedArr.pop(); // 오른쪽 제거
+      console.log('splitedArr:', splitedArr);
+    }
+  }
 };
