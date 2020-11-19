bd0fe9b00bab58105c022dbe34133f1525dcaaa0
preciouStar <seongjin605@users.noreply.github.com>
2020-11-19 23:08:29 +0900

    Create solution

---
+++ leetcode/decode-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function (s) {
+  const splitedArr = s.split('');
+  console.log('splitedArr:', splitedArr);
+  const result = [];
+};
