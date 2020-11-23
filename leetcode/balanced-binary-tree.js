ef3ea0bf72f575f2e0e38dd1539fb8b79b0b8fe9
preciouStar <seongjin605@users.noreply.github.com>
2020-11-23 22:19:46 +0900

    Create solution

---
+++ leetcode/balanced-binary-tree.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+  if (!root) return true;
+
+  const queue = [root];
+  let leftCount = 0;
+  let rightCount = 0;
+
+  const result = [];
+
+  while (queue.length > 0) {
+    const qSize = queue.length;
+    for (let i = 0; i < qSize; i++) {
+      const node = queue.shift();
+      console.log('node:', node);
+      if (node.left) {
+        queue.push(node.left);
+        leftCount++;
+      }
+      if (node.right) {
+        queue.push(node.right);
+        rightCount++;
+      }
+      result.push(node.val);
+    }
+  }
+
+  console.log('leftCount:', leftCount);
+  console.log('rightCount:', rightCount);
+
+  console.log('result:', result);
+};
