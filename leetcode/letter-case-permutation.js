05cdb23530bd5a1bd4c5823bf193094c606e0b2b
preciouStar <seongjin605@users.noreply.github.com>
2021-01-02 23:39:45 +0900

    check test case

--- leetcode/letter-case-permutation.js
+++ leetcode/letter-case-permutation.js
@@ -2,9 +2,30 @@
  * @param {string} S
  * @return {string[]}
  */
-const letterCasePermutation = function (S) {};
+const letterCasePermutation = function (S) {
+  const arr = S.split('');
+  const ret = [];
+  backtrack(arr, ret, 0);
+  return ret;
+};
 
-const isAlpha(str) {
+const backtrack = function (arr, ret, idx) {
+  if (idx === arr.length) {
+    ret.push(...arr);
+  } else {
+    const c = arr[idx];
+    if (isAlpha(c)) {
+      arr[idx] = c.toLowerCase();
+      backtrack(arr, ret, idx + 1);
+      arr[idx] = c.toUpperCase();
+      backtrack(arr, ret, idx + 1);
+    } else {
+      backtrack(arr, ret, idx + 1);
+    }
+  }
+};
+
+const isAlpha = function (c) {
   // TODO: 리턴 값에 입력받은 문자가 알파벳인지 체크하는 함수 구현.
-}
-
+  return ('a'.charAt() >= c && 'z'.charAt() <= c) || ('A'.charAt() >= c && 'Z'.charAt() <= c);
+};
