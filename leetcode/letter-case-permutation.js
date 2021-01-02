24fe7467545ea17bea0404ecdd295c8ce5847a7d
preciouStar <seongjin605@users.noreply.github.com>
2021-01-02 22:41:59 +0900

    Create alphabet check function

--- leetcode/letter-case-permutation.js
+++ leetcode/letter-case-permutation.js
@@ -2,4 +2,9 @@
  * @param {string} S
  * @return {string[]}
  */
-var letterCasePermutation = function (S) {};
+const letterCasePermutation = function (S) {};
+
+const isAlpha(str) {
+  // TODO: 리턴 값에 입력받은 문자가 알파벳인지 체크하는 함수 구현.
+}
+
