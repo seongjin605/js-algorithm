4a1fac61cc95df637239be56a174d5bc9dfeba3e
preciouStar <seongjin605@users.noreply.github.com>
2021-01-16 12:32:47 +0900

    Update backtracking

--- leetcode/letter-case-permutation.js
+++ leetcode/letter-case-permutation.js
@@ -1,15 +1,15 @@
 var letterCasePermutation = function (S) {
-  console.log('S:', S);
   return permut(S, 0);
 };
 
 function permut(str, pos, out = [], curr = '') {
+  // 재귀 종료 조건
   if (pos === str.length || curr.length === str.length) {
     out.push(curr);
   } else {
     if (isAlpha(str[pos])) {
-      let up = curr + str[pos].toUpperCase();
-      let low = curr + str[pos].toLowerCase();
+      const up = curr + str[pos].toUpperCase();
+      const low = curr + str[pos].toLowerCase();
       permut(str, pos + 1, out, up);
       permut(str, pos + 1, out, low);
     } else {
@@ -19,6 +19,7 @@ function permut(str, pos, out = [], curr = '') {
   return out;
 }
 
-function isAlpha(ch) {
-  return /[a-zA-Z]/i.test(ch);
+function isAlpha(c) {
+  const n = c.charCodeAt(0);
+  return (n >= 65 && n <= 90) || (n >= 97 && n <= 122);
 }
