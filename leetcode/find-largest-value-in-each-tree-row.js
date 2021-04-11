a9e5e4c82955678aea5d0c75938d5dcbc2f4ae97
preciouStar <seongjin605@users.noreply.github.com>
2021-04-11 23:05:13 +0900

    success

--- leetcode/find-largest-value-in-each-tree-row.js
+++ leetcode/find-largest-value-in-each-tree-row.js
@@ -7,19 +7,26 @@
  * }
  */
 /**
+ * https://leetcode.com/submissions/detail/479309542/
  * @param {TreeNode} root
  * @return {number[]}
  */
  var largestValues = function(root) {
-  if(!root) return [];
-  const queue = [root];
-  const result = [];
-  while(queue.length > 0) {
-      const node = queue.shift();
-          if(node) {
-              if(node.left) queue.push(node.left);
-              if(node.right) queue.push(node.right);
-              result.push(node.val)
-      }
-  }
+    if(!root) return [];
+    const queue = [root];
+    const result = [];
+    while(queue.length > 0) {
+        const currentLevel = [];
+        const size = queue.length;
+        for(let i = 0; i < size; i++) {
+            const node = queue.shift();
+            if(node) {
+                if(node.left) queue.push(node.left);
+                if(node.right) queue.push(node.right);
+                currentLevel.push(node.val)
+            }   
+        }
+        result.push(Math.max(...currentLevel));
+    }
+    return result;
 };
