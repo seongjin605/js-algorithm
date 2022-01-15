c861e22998073fdf4a4e04b6ee5e5d71a2428299
preciouStar <seongjin605@users.noreply.github.com>
2022-01-15 22:45:06 +0900

    update

--- leetcode/number-of-islands.js
+++ leetcode/number-of-islands.js
@@ -3,32 +3,25 @@
  * @return {number}
  */
 var numIslands = function (grid) {
-  if (!grid || grid.length === 0 || grid[0].length === 0) return 0;
-
-  let numIslands = 0;
-
-  for (let i = 0; i < grid.lengh; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      if (grid[i][j] === '1') {
-        numIslands += dfs(grid, i, j);
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] == '1') {
+        dfs(i, j, grid);
+        count += 1;
       }
     }
   }
-
-  return numIslands;
+  return count;
 };
 
-function dfs(grid, i, j) {
-  if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0') {
-    return;
-  }
+function dfs(i, j, grid) {
+  if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') return;
 
-  grid[i][j] = '0';
+  grid[i][j] = 0;
 
-  dfs(grid, i, j + 1); // right
-  dfs(grid, i, j - 1); // left
-  dfs(grid, i + 1, j); // up
-  dfs(grid, i - 1, j); // down
-
-  return 1;
+  dfs(i + 1, j, grid);
+  dfs(i - 1, j, grid);
+  dfs(i, j + 1, grid);
+  dfs(i, j - 1, grid);
 }
