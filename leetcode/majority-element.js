adbf473d34b7bc8aa3bc2bca48106eb129b5e176
Jin.Park <qkrtjdwls605@naver.com>
2022-01-26 14:35:29 +0900

    update

--- leetcode/majority-element.js
+++ leetcode/majority-element.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+var majorityElement = function (nums) {
   if (!nums || nums.length === 0) return 0;
 
   const map = new Map();
@@ -16,7 +16,7 @@ var majorityElement = function(nums) {
     }
   });
 
-  console.log("map:", map);
+  console.log('map:', map);
 
   const result = [];
   map.forEach((key, value) => {
@@ -25,4 +25,22 @@ var majorityElement = function(nums) {
   });
 
   return result.sort((a, b) => b.value - a.value)[0].key;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const key = nums[i];
+    if (!map.has(key)) {
+      map.set(key, 1);
+    } else {
+      map.set(key, map.get(key) + 1);
+    }
+  }
+  const sortedMap = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
+  return sortedMap.keys().next().value;
 };
