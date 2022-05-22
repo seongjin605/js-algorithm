8172cfbcab362dd03721d3d49132ea628ce6f905
preciouStar <seongjin605@users.noreply.github.com>
2022-05-22 23:40:35 +0900

    update

---
+++ leetcode/medium/keys-and-rooms.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function (rooms) {
+  const visited = new Array(rooms.length);
+  const dfs = index => {
+    if (visited[index]) return;
+
+    visited[index] = true;
+
+    const keys = rooms[index];
+    for (let key of keys) {
+      dfs(key);
+    }
+  };
+
+  dfs(0);
+
+  for (let visit of visited) {
+    if (!visit) return false;
+  }
+  return true;
+};
