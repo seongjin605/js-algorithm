7f0e3eb57e17e9a4656453eabaaf90a307dfdf58
Jin.Park <qkrtjdwls605@naver.com>
2022-08-05 13:45:42 +0900

    update

---
+++ leetcode/easy/linked-list-cycle.js
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode.com/problems/linked-list-cycle/
+ *
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      slow = head;
+      while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+      }
+      return true;
+    }
+  }
+  return false;
+};
