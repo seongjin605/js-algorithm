9c99939c4f27007bec7a804fd5597e5909a207e5
preciouStar <seongjin605@users.noreply.github.com>
2023-05-06 21:05:11 +0900

    update

--- leetcode/medium/group-anagrams.js
+++ leetcode/medium/group-anagrams.js
@@ -1,22 +1,39 @@
-const sortAlphabets = function (text) {
-  return text.split('').sort().join('');
-};
-
 /**
  * @param {string[]} strs
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-  let groups = [];
-  while (strs.length) {
-    const target = strs.pop();
-    const sortedStr = sortAlphabets(target);
-    const searchedIndex = groups.findIndex(g => sortAlphabets(g[0]) === sortedStr);
-    if (searchedIndex !== -1) {
-      groups[searchedIndex].push(target);
+  let map = {};
+  for (const str of strs) {
+    const key = str.split('').sort().join('');
+    if (!map[key]) {
+      map[key] = [str];
     } else {
-      groups.push([target]);
+      map[key].push(str);
     }
   }
-  return groups;
+
+  return Object.keys(map).map(key => map[key]);
 };
+// const sortAlphabets = function (text) {
+//   return text.split('').sort().join('');
+// };
+
+// /**
+//  * @param {string[]} strs
+//  * @return {string[][]}
+//  */
+// var groupAnagrams = function (strs) {
+//   let groups = [];
+//   while (strs.length) {
+//     const target = strs.pop();
+//     const sortedStr = sortAlphabets(target);
+//     const searchedIndex = groups.findIndex(g => sortAlphabets(g[0]) === sortedStr);
+//     if (searchedIndex !== -1) {
+//       groups[searchedIndex].push(target);
+//     } else {
+//       groups.push([target]);
+//     }
+//   }
+//   return groups;
+// };
